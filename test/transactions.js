fd3c3bdd13403f61608f7ce1615b676160860e5f
bkniffler <bkniffler@me.com>
2016-03-17 11:53:46 +0000

    Use ReactDOM.render

--- test/transactions.js
+++ test/transactions.js
@@ -45,7 +45,7 @@ tape.test("mobx issue 50", function(test) {
 	ReactDOM.render(React.createElement(Test), document.getElementById('testroot'));
 });
 
-tape.test("React.render should respect transaction", function(t) {
+tape.test("ReactDOM.render should respect transaction", function(t) {
 	var a = mobx.observable(2);
 	var loaded = mobx.observable(false);
 	var valuesSeen = [];
@@ -58,7 +58,7 @@ tape.test("React.render should respect transaction", function(t) {
 			return React.createElement("div", {}, "loading");
 	});
 	
-	React.render(React.createElement(component, {}), document.getElementById('testroot'));
+	ReactDOM.render(React.createElement(component, {}), document.getElementById('testroot'));
 	mobx.transaction(function() {
 		a.set(3);
 		a.set(4);
@@ -72,7 +72,7 @@ tape.test("React.render should respect transaction", function(t) {
 	}, 400);	
 });
 
-tape.test("React.render in transaction should succeed", function(t) {
+tape.test("ReactDOM.render in transaction should succeed", function(t) {
 	var a = mobx.observable(2);
 	var loaded = mobx.observable(false);
 	var valuesSeen = [];
@@ -86,7 +86,7 @@ tape.test("React.render in transaction should succeed", function(t) {
 	
 	mobx.transaction(function() {
 		a.set(3);
-		React.render(React.createElement(component, {}), document.getElementById('testroot'));
+		ReactDOM.render(React.createElement(component, {}), document.getElementById('testroot'));
 		a.set(4);
 		loaded.set(true);
 	});
@@ -126,7 +126,7 @@ tape.test("Should warn about changing state in getInitialState", function(t) {
 		baseError.apply(console, arguments);
 	};
 	
-	React.render(React.createElement(parent, {}), document.getElementById('testroot'), function() {
+	ReactDOM.render(React.createElement(parent, {}), document.getElementById('testroot'), function() {
 		console.error = baseError;
 		t.deepEqual(msg, [
 			"[mobx-react] Warning: A re-render was triggered before the component '<component>#.b.render()', was mounted. Is (another) component trying to modify state in it's constructor / getInitialState? Use componentWillMount instead.",
